1befdadc4e4d11f4cd4594994945e8e68a1b6975
Michael Milicevich <milicevichm@gmail.com>
2015-02-16 14:12:35 -0500

    Added disaggregation shell script

--- redd_conv.py
+++ redd_conv.py
@@ -3,5 +3,5 @@ from nilmtk.dataset_converters import convert_redd
 
 
 #convert data from redd into a .hdf file for future loading
-convert_redd("C:/NILM/Data_Sets/low_freq/","C:/Energy_Management/Data/redd_data.h5")
+convert_redd("C:/NILM/Data/REDD/low_freq/","C:/NILM/Data/Output/redd.h5")
 
