5e20086fcd921970429f5f3a51eef20c5523697b
Michael Milicevich <milicevichm@gmail.com>
2015-02-16 14:54:46 -0500

    Added relevant appliances to disag_script

--- disag_script.py
+++ disag_script.py
@@ -4,16 +4,22 @@
 
 # sys used to parse inoput arguments
 import sys
+from dateutil import parser
 
 # verify length of args, should be 5 corrsponding to:
 #[0]: script name: disag_script.py
-#[1]: REDD Building : arg_redd_building
-#[2]: Appliance to disaggregate: arg_appliances
-#[3]: Beginning time/date: arg_t1
-#[4]: Ending time/date: arg_t2
+#[1]: REDD Building : redd_building
+#[2]: Appliance to disaggregate: disag_appliances
+#[3]: Beginning time/date: t1
+#[4]: Ending time/date: t2
 
 # Verify that the correct amount of input arguments have been entered
 if len(sys.argv) != 5:
 	sys.exit("Error: Incorrect amount of input arguments given. Script terminated.")
 
+#load arguments into script
+redd_building = sys.argv[1]
+disag_appliances = sys.argv[2]
+t1 = parser.parse(sys.argv[3])
+t2 = parser.parse(sys.argv[4])
 
